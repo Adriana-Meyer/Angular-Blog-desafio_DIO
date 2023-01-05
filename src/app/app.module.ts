b44eeab0b298fe97390fff1d8a3dcc253c37dfa2
Adriana-Meyer <drika_meyer@hotmail.com>
2023-01-05 16:29:50 -0300

    Criando o componente de título principal (main-title)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MainTitleComponent } from './components/main-title/main-title.component';
+import { HighlightCardComponent } from './components/highlight-card/highlight-card.component';
+import { SideCardComponent } from './components/side-card/side-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainTitleComponent,
+    HighlightCardComponent,
+    SideCardComponent
   ],
   imports: [
     BrowserModule,
